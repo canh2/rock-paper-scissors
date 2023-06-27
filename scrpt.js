f90e294a4f7d6818780448f24fca8033ddae0ec5
canh2 <christina.g.amine@gmail.com>
2023-06-27 17:05:50 +0300

    computer choice added

--- scrpt.js
+++ scrpt.js
@@ -1,1 +1,21 @@
 console.log("first try");
+function getComputerChoice(){
+  var ch = Math.floor(Math.random()*3)+1;
+    var choice="";
+    switch (ch){
+        case 1:
+            choice="Rock";
+            break;
+         case 2:
+               choice="Paper";
+               break;
+           case 3:
+               choice="Scissors";
+               break;
+
+    }
+ 
+    return choice;
+    
+}
+console.log(getComputerChoice());
